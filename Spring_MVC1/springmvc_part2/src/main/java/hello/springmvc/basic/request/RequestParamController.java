da86ba61f551136047e047129afe394f00ddb506
k-ms1998 <k_ms1998@naver.com>
2022-02-03 09:18:53 +0900

    Using getParameter() to get request parameters

---
+++ Spring_MVC1/springmvc_part2/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -0,0 +1,27 @@
+package hello.springmvc.basic.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class RequestParamController {
+
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest req, HttpServletResponse res) throws IOException {
+//        using .getParameter()
+        String username = req.getParameter("username");
+        int age = Integer.parseInt(req.getParameter("age"));
+
+        log.info("username={}, age={}", username, age);
+
+        res.getWriter().write("ok");
+    }
+
+
+}
