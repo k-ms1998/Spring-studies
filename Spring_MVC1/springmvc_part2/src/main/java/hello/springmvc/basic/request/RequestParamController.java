6293e74d496a0cf5c29068c87b9f0f4f52d16555
k-ms1998 <k_ms1998@naver.com>
2022-02-03 09:27:29 +0900

    Using @RequestParam to get request parameters

--- Spring_MVC1/springmvc_part2/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ Spring_MVC1/springmvc_part2/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -3,6 +3,8 @@ package hello.springmvc.basic.request;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -23,5 +25,14 @@ public class RequestParamController {
         res.getWriter().write("ok");
     }
 
+    @ResponseBody
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(@RequestParam("username") String memberName,
+                               @RequestParam("age") int memberAge) {
+
+        log.info("username={}, age={}", memberName, memberAge);
+
+        return "ok"; // @ResponseBody => returns a string to response body instead of view resolver
+    }
 
 }
