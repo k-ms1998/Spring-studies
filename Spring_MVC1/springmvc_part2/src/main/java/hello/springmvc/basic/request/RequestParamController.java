bb72121815f267abb16e081f72542139cca787ef
k-ms1998 <k_ms1998@naver.com>
2022-02-03 10:08:20 +0900

    /request-param-required, /request-param-default, /request-param-map

--- Spring_MVC1/springmvc_part2/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ Spring_MVC1/springmvc_part2/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -28,7 +29,7 @@ public class RequestParamController {
     @ResponseBody
     @RequestMapping("/request-param-v2")
     public String requestParamV2(@RequestParam("username") String memberName,
-                               @RequestParam("age") int memberAge) {
+                                 @RequestParam("age") int memberAge) {
 
         log.info("username={}, age={}", memberName, memberAge);
 
@@ -54,6 +55,40 @@ public class RequestParamController {
         log.info("username={}, age={}", username, age);
 
         return "ok"; // @ResponseBody => returns a string to response body instead of view resolver
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(@RequestParam(required = true) String username,
+                                       @RequestParam(required = false) Integer age) {
+//  required=true -> Parameter is required
+//  Therefore, /request-param-required?username=hello => ok, /request-param-required?age=20 => error(400)
+        log.info("username={}, age={}", username, age);
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(@RequestParam(required = true, defaultValue = "guest") String username,
+                                      @RequestParam(required = false, defaultValue = "-1") Integer age) {
+//  /request-param-default?age=20 -> ok, because username will default to defaultValue
+//  /request-param-default => ok, username=guest, age=-1
+//        => required can be ignored if there is defaultValue
+        log.info("username={}, age={}", username, age); // =>username=guest, age=20
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
+//  Getting all parameters as a Map
+//  /request-param-map?username=hello&age=20&grade=vip
+        paramMap.forEach((k,v) -> log.info("paramName={}, paramValue={}", k, v));
+        log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+
+        return "ok";
     }
 
 }
