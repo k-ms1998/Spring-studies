aad3654078385e62583f994203ee395c22315c54
k-ms1998 <k_ms1998@naver.com>
2022-02-11 16:54:32 +0900

    Thymeleaf fragment

---
+++ Spring_MVC2/SpringMVC2_part1/src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -0,0 +1,17 @@
+package hello.thymeleaf.basic;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/template")
+public class TemplateController {
+
+    @GetMapping("/fragment")
+    public String fragment() {
+        return "template/fragment/fragmentMain";
+    }
+
+}
