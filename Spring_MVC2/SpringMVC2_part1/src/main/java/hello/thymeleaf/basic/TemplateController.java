b21609be8e30d2496da722346a77a0842fed309f
k-ms1998 <k_ms1998@naver.com>
2022-02-14 09:39:30 +0900

    Thymeleaf template

--- Spring_MVC2/SpringMVC2_part1/src/main/java/hello/thymeleaf/basic/TemplateController.java
+++ Spring_MVC2/SpringMVC2_part1/src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -14,4 +14,14 @@ public class TemplateController {
         return "template/fragment/fragmentMain";
     }
 
+    @GetMapping("/layout")
+    public String layout() {
+        return "template/layout/layoutMain";
+    }
+
+    @GetMapping("/layoutExtend")
+    public String layoutExtend() {
+        return "template/layoutExtend/layoutExtendMain";
+    }
+
 }
