74d3557dc239cb9e55462a7c381de5f72f27cf8d
k-ms1998 <k_ms1998@naver.com>
2022-02-18 17:44:00 +0900

    Bean Validation Form 객체 분리

--- Spring_MVC2/SpringMVC2_part4/src/main/java/hello/itemservice/domain/item/Item.java
+++ Spring_MVC2/SpringMVC2_part4/src/main/java/hello/itemservice/domain/item/Item.java
@@ -10,19 +10,19 @@ import javax.validation.constraints.NotNull;
 @Data
 public class Item {
 
-    @NotNull(groups = UpdateCheck.class)
+//    @NotNull(groups = UpdateCheck.class)
     private Long id;
 
-    @NotBlank(message="공백 X", groups = {SaveCheck.class, UpdateCheck.class})   //해당 오류가 발생했을때 message="" 으로 오류 메세지 직접 설정 가능
-    @NotNull
+//    @NotBlank(message="공백 X", groups = {SaveCheck.class, UpdateCheck.class})   //해당 오류가 발생했을때 message="" 으로 오류 메세지 직접 설정 가능
+//    @NotNull
     private String itemName;
 
-    @NotNull(message="가격은 필수", groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min = 1000, max = 1000000)   //message 설정안하면 자동으로 설정해줌
+//    @NotNull(message="가격은 필수", groups = {SaveCheck.class, UpdateCheck.class})
+//    @Range(min = 1000, max = 1000000)   //message 설정안하면 자동으로 설정해줌
     private Integer price;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Max(value = 10000, groups = {SaveCheck.class}) //10000을 초과하면 오류
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Max(value = 10000, groups = {SaveCheck.class}) //10000을 초과하면 오류
     private Integer quantity;
 
     public Item() {
