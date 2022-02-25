9eec6185b8dbab718233be31635b3431f1b845ac
k-ms1998 <k_ms1998@naver.com>
2022-02-25 12:14:41 +0900

    Spring API Exception Handling

---
+++ Spring_MVC2/SpringMVC2_part6(Exceptions)/src/main/java/hello/exception/api/ApiExceptionController.java
@@ -0,0 +1,45 @@
+package hello.exception.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class ApiExceptionController {
+
+    @GetMapping("/api/members/{id}")
+    public MemberDto getMember(@PathVariable("id") String id) {
+        if (id.equals("ex")) {
+            /**
+             * {
+             *     "timestamp": "2022-02-25T02:51:29.234+00:00",
+             *     "status": 500,
+             *     "error": "Internal Server Error",
+             *     "path": "/api/members/ex"
+             * }
+             */
+            throw new RuntimeException("잘못된 사용자 ApiExceptionController");
+        }
+
+        return new MemberDto(id, "hello" + id);
+        /**
+         * @returns
+         * {
+         *     "memberId":id,
+         *     "name": "hello"+id
+         * }
+         */
+    }
+
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String memberId;
+        private String name;
+    }
+}
--- Spring_MVC2/SpringMVC2_part6(Exceptions)/src/main/java/hello/exception/servlet/ErrorPageController.java
+++ Spring_MVC2/SpringMVC2_part6(Exceptions)/src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -1,12 +1,21 @@
 package hello.exception.servlet;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+import static javax.servlet.RequestDispatcher.ERROR_EXCEPTION;
+import static javax.servlet.RequestDispatcher.ERROR_STATUS_CODE;
 
 @Slf4j
 @Controller
@@ -28,5 +37,23 @@ public class ErrorPageController {
         return "/error-page/500";
     }
 
+    //같은 "/error/500" 에 대한 request여도,
+    // request시, Header에서 Accept 필드의 값이 application/json 이면,
+    // produces의 값과 Accept 필드의 값이 일치하는 request가 우선 순위를 갖는다
+    // => 충돌 X
+    @RequestMapping(value = "/500", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, Object>> errorPage500Api(
+            HttpServletRequest request, HttpServletResponse response
+    ) {
+        log.info("API errorPage500");
 
+        Map<String, Object> result = new HashMap<>();
+        Exception ex = (Exception) request.getAttribute(ERROR_EXCEPTION);
+        result.put("status", request.getAttribute(ERROR_STATUS_CODE));
+        result.put("message", ex.getMessage());
+
+        Integer statusCode = (Integer) request.getAttribute(ERROR_STATUS_CODE);
+
+        return new ResponseEntity<>(result, HttpStatus.valueOf(statusCode));
+    }
 }
