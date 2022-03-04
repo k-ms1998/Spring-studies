b1e8b3d3ed8d038db16d7679d0a3e7f8d1bdf574
k-ms1998 <k_ms1998@naver.com>
2022-03-04 16:24:55 +0900

    Spring TypeConverter ConversionService

--- Spring_MVC2/SpringMVC2_part7(TypeConverter)/src/main/java/hello/typeconverter/controller/HelloController.java
+++ Spring_MVC2/SpringMVC2_part7(TypeConverter)/src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,5 +1,6 @@
 package hello.typeconverter.controller;
 
+import hello.typeconverter.type.IpPort;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,9 +22,21 @@ public class HelloController {
         return "ok";
     }
 
-    @GetMapping("/hello-v2")
+    @GetMapping("/hello-v2") // localhost:8080/hello-v2?data1=10 => data == "10"
     public String helloV2(@RequestParam Integer data) {
+//        registry에 StringToIntegerConverter 추가시 알아서 String => Integer로 변환 해줌
+
         log.info("data={}", data);
+
+        return "ok";
+    }
+
+    @GetMapping("/ip-port") //localhost:8080/ip-port?ipPort=127.0.0.1:8080 => ipPort == "127.0.0.1:8080"
+    public String ipPort(@RequestParam IpPort ipPort) {
+        //registry에 StringToIpPortConverter 등록 => ipPort String => IpPort로 변환
+
+        log.info("IpPort Ip={}", ipPort.getIp());
+        log.info("IpPort Port={}", ipPort.getPort());
 
         return "ok";
     }
---
+++ Spring_MVC2/SpringMVC2_part7(TypeConverter)/src/test/java/hello/typeconverter/converter/ConversionServiceTest.java
@@ -0,0 +1,29 @@
+package hello.typeconverter.converter;
+
+import hello.typeconverter.type.IpPort;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.convert.support.DefaultConversionService;
+
+public class ConversionServiceTest {
+
+    @Test
+    void conversionService() {
+        //등록
+        DefaultConversionService conversionService = new DefaultConversionService();
+
+        conversionService.addConverter(new StringToIntegerConverter());
+        conversionService.addConverter(new IntegerToStringConverter());
+        conversionService.addConverter(new StringToIpPortConverter());
+        conversionService.addConverter(new IpPortToStringConverter());
+
+        //사용
+//        conversionService.convert(source, target type)
+        Assertions.assertThat(conversionService.convert("10", Integer.class)).isEqualTo(10);
+        Assertions.assertThat(conversionService.convert(10, String.class)).isEqualTo("10");
+        Assertions.assertThat(conversionService.convert("127.0.0.1:8080", IpPort.class)).isEqualTo(new IpPort("127.0.0.1", 8080));
+        Assertions.assertThat(conversionService.convert(new IpPort("127.0.0.1", 8080), String.class)).isEqualTo("127.0.0.1:8080");
+
+    }
+
+}
