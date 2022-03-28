00579aa9c36613b56dc4be924698a6ce732af95a
k-ms1998 <k_ms1998@naver.com>
2022-03-28 13:24:19 +0900

    Spring Advanced Part3 AOP Pointcuts

--- Spring_advanced/part3_aop/src/main/java/hello/aop/AopApplication.java
+++ Spring_advanced/part3_aop/src/main/java/hello/aop/AopApplication.java
@@ -3,13 +3,15 @@ package hello.aop;
 import hello.aop.order.aop.AspectV1;
 import hello.aop.order.aop.AspectV2;
 import hello.aop.order.aop.AspectV3;
+import hello.aop.order.aop.AspectV4;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 //@Import(AspectV1.class)
 //@Import(AspectV2.class)
-@Import(AspectV3.class)
+//@Import(AspectV3.class)
+@Import(AspectV4.class)
 @SpringBootApplication
 public class AopApplication {
 
---
+++ Spring_advanced/part3_aop/src/main/java/hello/aop/order/aop/AspectV4.java
@@ -0,0 +1,36 @@
+package hello.aop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Slf4j
+@Aspect
+public class AspectV4 {
+
+    @Around("hello.aop.order.aop.Pointcuts.allOrder()") // OrderRepository && OrderService 모두 적용
+    public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("[AspectV4 log] {}", joinPoint.getSignature()); //join point 시그니처
+        return joinPoint.proceed();
+    }
+
+    //hello.aop.order 패키지와 하위 패키지 이면서 클래스 이름 패턴이 *Service => 그러므로, OrderSerivce 에만 적용됨
+    @Around("hello.aop.order.aop.Pointcuts.orderAndService()")
+    public Object doTransaction(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        try {
+            log.info("[AspectV4 트랜잭션 시작] {}", joinPoint.getSignature());
+            Object result = joinPoint.proceed();
+            log.info("[AspectV4 트랜잭션 커밋] {}", joinPoint.getSignature());
+            return result;
+        } catch (Exception e) {
+            log.info("[AspectV4 트랜잭션 롤백] {}", joinPoint.getSignature());
+            throw e;
+        } finally {
+            log.info("[AspectV4 리소스 릴리즈] {}", joinPoint.getSignature());
+        }
+    }
+
+}
