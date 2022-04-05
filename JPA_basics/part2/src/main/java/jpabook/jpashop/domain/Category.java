5298bef7601144c767cebdedca93490aaaf8f894
k-ms1998 <k_ms1998@naver.com>
2022-04-05 17:27:09 +0900

    JPA Basics 다양한 연관관계 매핑 (다대다 N:M 관계 주석 추가)

--- JPA_basics/part2/src/main/java/jpabook/jpashop/domain/Category.java
+++ JPA_basics/part2/src/main/java/jpabook/jpashop/domain/Category.java
@@ -17,6 +17,12 @@ public class Category {
     @JoinColumn(name = "PARENT_ID")
     private Category parent;
 
+    /**
+     * 다대다(N:M) 관계는 사용 추천 X && 실무에서 사용 X
+     * 중간 테이블(category_item)을 이용해서 item과 category 간 다대다 연관관계 성립
+     * But, 실전에서는 중간 테이블에 연결할때 필요한 정보 외에 다른 정보들이 추가되는 경우가 많음
+     * 그렇기 때문에, 실무에서는 살용적이지 않음
+     */
     @ManyToMany
     @JoinTable(name = "CATEGORY_ITEM",
             joinColumns = @JoinColumn(name = "CATEGORY_ID"),
