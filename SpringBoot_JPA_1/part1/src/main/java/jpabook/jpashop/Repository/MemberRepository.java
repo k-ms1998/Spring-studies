020db9fe80c4754190b1a2346977bbf6ac9887da
k-ms1998 <k_ms1998@naver.com>
2022-04-13 16:22:38 +0900

    SpringBoot & JPA Part1 MemberRepository

--- SpringBoot_JPA_1/part1/src/main/java/jpabook/jpashop/Repository/MemberRepository.java
+++ SpringBoot_JPA_1/part1/src/main/java/jpabook/jpashop/Repository/MemberRepository.java
@@ -5,7 +5,11 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
+/**
+ * Repository: JPA를 직접 사용하는 계층, 엔티티 매니저 사용
+ */
 @Repository
 public class MemberRepository {
 
@@ -20,5 +24,16 @@ public class MemberRepository {
 
     public Member find(Long id) {
         return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.username = :userName", Member.class)
+                .setParameter("userName", name)
+                .getResultList();
     }
 }
