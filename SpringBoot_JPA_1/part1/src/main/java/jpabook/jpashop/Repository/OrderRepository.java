62a51b978f6ae3bee150312f96de0a5ab3578c0f
k-ms1998 <k_ms1998@naver.com>
2022-04-14 16:38:13 +0900

    SpringBoot & JPA Part1 OrderService, OrderRepository, OrderServiceTest

---
+++ SpringBoot_JPA_1/part1/src/main/java/jpabook/jpashop/Repository/OrderRepository.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.Repository;
+
+import jpabook.jpashop.Domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+}
