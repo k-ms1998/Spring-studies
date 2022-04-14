7cdcd02a2ad02ee339d1ad33a7e684a6c93d2a8a
k-ms1998 <k_ms1998@naver.com>
2022-04-14 17:47:13 +0900

    SpringBoot & JPA Controller Getting Started

---
+++ SpringBoot_JPA_1/part1/src/main/java/jpabook/jpashop/Controller/HomeController.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop.Controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        log.info("Home Controller");
+        return "home";
+    }
+
+
+
+
+
+}
