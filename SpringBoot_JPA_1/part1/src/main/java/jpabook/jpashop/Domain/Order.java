a78b50e885b4ec0f3835060b85fc299a00714cc9
k-ms1998 <k_ms1998@naver.com>
2022-04-14 12:54:40 +0900

    SpringBoot & JPA Part1 Order&OrderItem 비즈니스 로직

--- SpringBoot_JPA_1/part1/src/main/java/jpabook/jpashop/Domain/Order.java
+++ SpringBoot_JPA_1/part1/src/main/java/jpabook/jpashop/Domain/Order.java
@@ -6,7 +6,6 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -49,4 +48,62 @@ public class Order {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
+
+    /**
+     * ==생성 메서드==
+     * 생성자 대신 생성 메서드를 사용하는 이유는 정적 팩토리 메서드 패턴을 만족시키기 위함입니다.
+     */
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        //OrderItem 생성 -> createOrderItem() 호출 -> Order 생성 -> createOrder() 호출
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+
+        return order;
+    }
+
+    /**
+     * ==비즈니스 로직==
+     */
+    // =주문 취소=
+    public void cancelOrder() {
+        //이미 배달 완료이면 취소가 불가능
+        if (this.getDelivery().getStatus() == DeliveryStatus.COMPLETE) {
+            throw new IllegalStateException("이미 배송이 완료된 상품은 취소가 불가능");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : this.getOrderItems()) {
+            //주문한 상품의 주문을 취소하면, 취소한 만큼 재고 수량을 변화 시켜줘야됨
+            orderItem.cancel();
+        }
+    }
+
+    /**
+     * ==조회 로직==
+     */
+    // =전체 주문 가격 조회=
+    public int getTotalPrice() {
+//        int totalPrice = 0;
+//        for (OrderItem orderItem : this.getOrderItems()) {
+//            totalPrice += orderItem.calculateTotalPrice();
+//        }
+//        return totalPrice;
+
+        return this.getOrderItems().stream()
+                .mapToInt((o) -> {
+                    return o.calculateTotalPrice();
+                }).sum();
+
+    }
+    /**
+     * 직접 필드를 접근하지 않고 Getter를 사용하는 이유는 OrderItem 하단 부분 주석 확인
+     */
+
 }
+
--- SpringBoot_JPA_1/part1/src/main/java/jpabook/jpashop/Domain/OrderItem.java
+++ SpringBoot_JPA_1/part1/src/main/java/jpabook/jpashop/Domain/OrderItem.java
@@ -3,6 +3,7 @@ package jpabook.jpashop.Domain;
 import jpabook.jpashop.Domain.item.Item;
 import lombok.Getter;
 import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
 
 import javax.persistence.*;
 
@@ -28,4 +29,33 @@ public class OrderItem {
     private int count; //주문 수량
 
 
+    /**
+     * 생성 메서드
+     */
+    static public OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice); //상품에 대한 할인 등이 적용 될 수도 있기 때문에 item.getPrice() 대신 orderPrice를 인자로 받아서 set
+        orderItem.setCount(count);
+
+        item.decreaseStock(count);
+
+        return orderItem;
+    }
+
+    /**
+     * 비즈니스 로직
+     */
+    public void cancel() {
+        //주문 수량만큼 상품의 재고 수량을 추가
+        this.getItem().increaseStock(this.getCount());
+    }
+
+    public int calculateTotalPrice() {
+        return this.getOrderPrice()*this.getCount();
+    }
+    /**
+     * 직접 필드에 접근(ex: orderPrice*count)를 하지 않고, Getter로 필드를 접근하는 이유는 프록시 객체를 이용 했을때, 프록시 객체에서는 필드의 값이 null이 반환 됩니다.
+     * 그러므로, Getter를 사용해서 프록시 객체가 아닌 원본 객체에서의 필드 값들을 가죠오게 합니다.
+     */
 }
