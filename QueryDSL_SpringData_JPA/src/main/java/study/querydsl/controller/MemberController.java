4cd516130e8fa53d359b7dadb97dd8c42e1acd69
k-ms1998 <k_ms1998@naver.com>
2022-05-09 12:18:55 +0900

    JPA QueryDSL Advanced Part 4: SpringData + QueryDSL + Pageable Result

--- QueryDSL_SpringData_JPA/src/main/java/study/querydsl/controller/MemberController.java
+++ QueryDSL_SpringData_JPA/src/main/java/study/querydsl/controller/MemberController.java
@@ -1,6 +1,8 @@
 package study.querydsl.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import study.querydsl.dto.MemberSearchCondition;
@@ -27,4 +29,59 @@ public class MemberController {
     public List<MemberTeamDTO> searchMemberV2(MemberSearchCondition condition) {
         return repository.search(condition);
     }
+
+    // /v3/members?teamName=TeamA&page=1&size=5 -> teamName => condition, page && size => pageable
+    @GetMapping("/v3/members")
+    public Page<MemberTeamDTO> searchMemberV3(MemberSearchCondition condition, Pageable pageable) {
+        return repository.searchPageable(condition, pageable);
+        /**
+         * 결과:
+         * {
+         *     "content": [
+         *         {
+         *             "memberId": 13,
+         *             "username": "member10",
+         *             "age": 10,
+         *             "teamId": 1,
+         *             "teamName": "TeamA"
+         *         },
+         *         (생략)
+         *         {
+         *             "memberId": 21,
+         *             "username": "member18",
+         *             "age": 18,
+         *             "teamId": 1,
+         *             "teamName": "TeamA"
+         *         }
+         *     ],
+         *     "pageable": {
+         *         "sort": {
+         *             "empty": true,
+         *             "sorted": false,
+         *             "unsorted": true
+         *         },
+         *         "offset": 5,
+         *         "pageNumber": 1,
+         *         "pageSize": 5,
+         *         "paged": true,
+         *         "unpaged": false
+         *     },
+         *     "last": false,
+         *     "totalElements": 50,
+         *     "totalPages": 10,
+         *     "number": 1,
+         *     "size": 5,
+         *     "sort": {
+         *         "empty": true,
+         *         "sorted": false,
+         *         "unsorted": true
+         *     },
+         *     "first": false,
+         *     "numberOfElements": 5,
+         *     "empty": false
+         * }
+         * => V2와 비교 했을때 Paging에 대한 값들도 같이 반환됨
+         */
+    }
+
 }
