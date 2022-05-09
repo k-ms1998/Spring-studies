735c3343ff0a718dd304e5cb95b2952b88ba6eee
k-ms1998 <k_ms1998@naver.com>
2022-05-09 09:22:33 +0900

    JPA QueryDSL Advanced Part 2: Get API + Search Condition

---
+++ QueryDSL_SpringData_JPA/src/main/java/study/querydsl/InitMember.java
@@ -0,0 +1,49 @@
+package study.querydsl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
+import study.querydsl.repository.MemberRepositoryQueryDSL;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Profile("local")
+@Component
+@RequiredArgsConstructor
+public class InitMember {
+
+    private final InitMemberService initMemberService;
+
+    @PostConstruct
+    public void init() {
+        initMemberService.init();
+    }
+
+    @Component
+    static class InitMemberService {
+        @PersistenceContext
+        private EntityManager em;
+
+        @Transactional
+        public void init() {
+            Team teamA = new Team("TeamA");
+            Team teamB = new Team("TeamB");
+
+            em.persist(teamA);
+            em.persist(teamB);
+
+            for (int i = 0; i < 100; i++) {
+                Team team = i % 2 == 0 ? teamA : teamB;
+                em.persist(new Member("member" + i, i, team));
+            }
+        }
+
+    }
+
+
+}
