bb4078ebce26ef306821815a415cd5f3326f641a
Min Seop Kim <k_ms1998@naver.com>
2022-11-19 23:56:47 +0900

    fc_notice_board Initial Commit

---
+++ fc_notice_board/src/main/java/com/fc/notice_board/notice_board/NoticeBoardApplication.java
@@ -0,0 +1,13 @@
+package com.fc.notice_board.notice_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticeBoardApplication.class, args);
+	}
+
+}
---
+++ fc_notice_board/src/test/java/com/fc/notice_board/notice_board/NoticeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fc.notice_board.notice_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
