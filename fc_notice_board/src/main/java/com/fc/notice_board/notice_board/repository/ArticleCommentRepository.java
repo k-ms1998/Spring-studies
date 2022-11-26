8ac20ecf59d2887f7a474d29d23beb650eee8ef4
Min Seop Kim <k_ms1998@naver.com>
2022-11-26 23:05:24 +0900

    Add #1:  Applying Spring Data REST

--- fc_notice_board/src/main/java/com/fc/notice_board/notice_board/repository/ArticleCommentRepository.java
+++ fc_notice_board/src/main/java/com/fc/notice_board/notice_board/repository/ArticleCommentRepository.java
@@ -2,6 +2,8 @@ package com.fc.notice_board.notice_board.repository;
 
 import com.fc.notice_board.notice_board.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
