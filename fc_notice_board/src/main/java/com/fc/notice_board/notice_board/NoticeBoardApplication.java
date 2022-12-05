51943cc0e163f8508d2a30756770df45f09655d1
Min Seop Kim <k_ms1998@naver.com>
2022-12-05 16:04:54 +0900

    Add #9: Thymeleaf Decoupled logic 적용

--- fc_notice_board/src/main/java/com/fc/notice_board/notice_board/NoticeBoardApplication.java
+++ fc_notice_board/src/main/java/com/fc/notice_board/notice_board/NoticeBoardApplication.java
@@ -2,8 +2,10 @@ package com.fc.notice_board.notice_board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class NoticeBoardApplication {
 
 	public static void main(String[] args) {
